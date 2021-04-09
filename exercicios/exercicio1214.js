02489aa7cac92b4fb9c4da1dccf49ab995ec699d
SilasHikaru <silashiuga@hotmail.com>
2021-04-09 12:49:01 -0300

    add pipe

--- exercicios/exercicio1214.js
+++ exercicios/exercicio1214.js
@@ -10,8 +10,8 @@ function exercicio(input) {
 
     // para utilizar a entrada como uma string para facilitar no envio por parâmetro da api, alterei o split('/n') para split(" ")
     // o comentário abaixo é um exemplo de entrada adaptado 
-    // var input = "5 5 50 50 70 80 100 7 100 95 90 80 70 60 50 3 70 90 80 3 70 90 81 9 100 99 98 97 96 95 94 93 91"
-    var lines = input.split(" ");
+    // var input = "5|5|50|50|70|80|100|7|100|95|90|80|70|60|50|3|70|90|80|3|70|90|81|9|100|99|98|97|96|95|94|93|91"
+    var lines = input.split("|");
     console.log(lines);
     var qtd = parseInt(lines[0]);
     lines.shift();
