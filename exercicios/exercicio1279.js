d42adb8f4a70c18cbb4bcfe88bf7cd7f13c31992
Nathan Reis <nathansilvareis@gmail.com>
2021-04-08 22:59:23 -0300

    Correcao

--- exercicios/exercicio1279.js
+++ exercicios/exercicio1279.js
@@ -1,27 +1,23 @@
 function exercicio1279(ano) {
     var results = [];
 
-    if (ano % 4 == 0 && ano % 400 == 0 && ano % 100 != 0) {
-        results[0] = "This is leap year.";
+    if (ano % 4 == 0 && (ano % 400 == 0 || ano % 100 != 0)) {
+        results.push("This is leap year.");
 
         if (ano % 55 == 0) {
-            results[2] = "This is bulukulu festival year.";
+            results.push("This is bulukulu festival year.");
         }
     }
 
     if (ano % 15 == 0) {
-        results[1] = "This is huluculu festival year.";
+        results.push("This is huluculu festival year.");
     }
 
-    var maxResults = results.length;
-
-    for (var x = 0; x < maxResults; x++) {
-        console.log(results[x]);
+    if (results.length == 0) {
+        results.push("This is an ordinary year.");
     }
 
-    if (!results) {
-        console.log("This is an ordinary year.");
-    }
+    return results;
 }
 
 module.exports = exercicio1279;
